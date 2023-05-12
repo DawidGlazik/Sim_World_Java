3ce56900a65ad827d553c704e7d107a260071596
GandalfSzaray <dawid-glazik@wp.pl>
2023-05-12 19:13:41 +0200

    Cosmetics

---
+++ Main.java
@@ -0,0 +1,12 @@
+import swiat.Swiat;
+public class Main {
+	
+	private final static int kolumny = 20;
+	private final static int wiersze = 10;
+	
+	public static void main(String[] args) {
+		Swiat swiat = new Swiat(kolumny,wiersze);
+		swiat.rysujSwiat();		
+	}
+
+}
